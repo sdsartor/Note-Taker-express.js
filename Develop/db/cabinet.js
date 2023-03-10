5ad978001ec61a526dba746b2b03c088635f3367
sdsartor <sdsartor48@gmail.com>
2023-03-09 18:48:24 -0800

    cleaned up the unnecessary code that has no purpose in the project

--- Develop/db/cabinet.js
+++ Develop/db/cabinet.js
@@ -1,17 +1,2 @@
-const uuid = require('uuid').v1;
-const fs = require('fs');
-const util = require('util');
 
-const readFromFile = util.promisify(fs.readFile);
-const writeToFile = util.promisify(fs.writeFile);
-
-class cabinet {
-    readNotes() {
-        return readFromFile('../db/db.json', JSON.stringify(data));
-    }
-    writeNotes(note) {
-        return writeToFile('db/db.json', JSON.stringify(data))
-    }
-    plusNotes(data) {
-     
-    }};
+// None of this code has an impact on the lesson, but can be used to expand the database functionality.
--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const storage = require('../db/db.json');
-
+const express = require('express');
+const DB = require('../db/db.json')
 
 router.get('/', (req, res) => {
     storage
@@ -13,23 +14,31 @@ router.get('/', (req, res) => {
     })
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    storage
-    .addNote(req.body)
-    .then(note => {
-        res.json(note)
-    })
-    .catch(err => {
-        res.status(404).json(err)
-    })
-})
+router.post("/api/notes", async function (req, res) {
+    const currentNotes = await DB.readNotes();
+    let newNote = {
+      id: uuid(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+  
+    await DB.addNote([...currentNotes, newNote]);
+  
+    return res.send(newNote);
+  });
 
-router.delete('/:id', (req, res) => {
-    storage
-    .deleteNote(req.params.id)
-    .then(() => res.json({ ok:true }))
-    .catch(err => res.status(404).json(err))
-})
-
+router.delete("/api/notes/:id", async function (req, res) {
+    // separates out the note to delete based on id
+    const noteToDelete = req.params.id;
+    // notes already in json file
+    const currentNotes = await DB.readNotes();
+    // sort through notes file and create a new array minus the note in question
+    const newNoteData = currentNotes.filter((note) => note.id !== noteToDelete);
+  
+    // sends the new array back the DB class 
+    await DB.deleteNote(newNoteData);
+    
+    return res.send(newNoteData);
+  });
+  
 module.exports = router;
